7f2385f31a8724817b116df0d488e654256be41e
jeremy <jeremyeparker@yahoo.com>
2021-12-01 01:00:11 -0500

    add GET all and POST routes

--- src/main/java/com/atsignJar/capricious/controllers/BookController.java
+++ src/main/java/com/atsignJar/capricious/controllers/BookController.java
@@ -1,10 +1,13 @@
 package com.atsignJar.capricious.controllers;
 
+import com.atsignJar.capricious.models.Book;
 import com.atsignJar.capricious.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -13,6 +16,17 @@ public class BookController {
 
     @Autowired
     private BookRepository repository;
+
+    //Get All Books
+    @GetMapping
+    public @ResponseBody
+    List<Book> getBooks(){return repository.findAll();}
+
+    //Create one
+    @PostMapping
+    public ResponseEntity<Book> createCustomer(@RequestBody Book newBook){
+        return new ResponseEntity<>(repository.save(newBook), HttpStatus.CREATED);
+    }
 
 
 }
