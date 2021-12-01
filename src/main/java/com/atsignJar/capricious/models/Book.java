cf696592f74dfbd4413859f4f5b44b26839182cc
jeremy <jeremyeparker@yahoo.com>
2021-12-01 01:00:54 -0500

    constructors and setters/getters

--- src/main/java/com/atsignJar/capricious/models/Book.java
+++ src/main/java/com/atsignJar/capricious/models/Book.java
@@ -8,8 +8,36 @@ import javax.persistence.Id;
 public class Book {
     private String title;
     private String author;
-    private int year;
-    @Id
-    @GeneratedValue
-    private Long id;
+    @Id @GeneratedValue private Long id;
+
+    public Book(){
+
+    }
+
+    public Book(String title,String author){
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
 }
