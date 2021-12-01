783259c4ab50a5300ad8397506ad03d8139036b1
jeremy <jeremyeparker@yahoo.com>
2021-11-30 23:49:41 -0500

    demo model

--- src/main/java/com/atsignJar/capricious/models/Book.java
+++ src/main/java/com/atsignJar/capricious/models/Book.java
@@ -1,2 +1,15 @@
-package com.atsignJar.capricious.models;public class Book {
+package com.atsignJar.capricious.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+    @Id
+    @GeneratedValue
+    private Long id;
 }
