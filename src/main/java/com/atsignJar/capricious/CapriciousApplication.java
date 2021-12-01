f5eaea5e877568664eed29853841035c37a6bd89
jeremy <jeremyeparker@yahoo.com>
2021-11-30 23:41:31 -0500

    first commit

---
+++ src/main/java/com/atsignJar/capricious/CapriciousApplication.java
@@ -0,0 +1,13 @@
+package com.atsignJar.capricious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapriciousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapriciousApplication.class, args);
+	}
+
+}
