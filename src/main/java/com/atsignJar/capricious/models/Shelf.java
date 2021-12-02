99d97fa0b72b0cf26c92be2d506bd2a5d11323c6
Jermy <jeremyeparker@yahoo.com>
2021-12-02 13:31:02 -0500

    broken and commented atm

--- src/main/java/com/atsignJar/capricious/models/Shelf.java
+++ src/main/java/com/atsignJar/capricious/models/Shelf.java
@@ -1,39 +1,39 @@
-package com.atsignJar.capricious.models;
-
-import javax.persistence.*;
-import java.util.List;
-
-@Entity
-public class Shelf {
-    @Id
-    @GeneratedValue
-    private Long id;
-    private String name;
-    private List <Book> books;
-
-
-    public Shelf(){
-    }
-
-    public Shelf(String name) {
-        this.name = name;
-    }
-
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
+//package com.atsignJar.capricious.models;
+//
+//import javax.persistence.*;
+//import java.util.List;
+//
+//@Entity
+//public class Shelf {
+//    @Id
+//    @GeneratedValue
+//    private Long id;
+//    private String name;
+//    private List <Book> books;
+//
+//
+//    public Shelf(){
+//    }
+//
+//    public Shelf(String name) {
+//        this.name = name;
+//    }
+//
+//
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//}
--- src/main/java/com/atsignJar/capricious/repositories/ShelfRepository.java
+++ src/main/java/com/atsignJar/capricious/repositories/ShelfRepository.java
@@ -1,7 +1,7 @@
-package com.atsignJar.capricious.repositories;
-
-import com.atsignJar.capricious.models.Shelf;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShelfRepository extends JpaRepository<Shelf,Long> {
-}
+//package com.atsignJar.capricious.repositories;
+//
+//import com.atsignJar.capricious.models.Shelf;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ShelfRepository extends JpaRepository<Shelf,Long> {
+//}//
