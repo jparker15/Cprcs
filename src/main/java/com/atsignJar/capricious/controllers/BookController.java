075452bba33c2d07c29a7a4f893d993c0774717b
Jeremy <jeremyeparker@yahoo.com>
2021-12-02 19:31:48 -0500

    add missing CRUD routes

--- src/main/java/com/atsignJar/capricious/controllers/BookController.java
+++ src/main/java/com/atsignJar/capricious/controllers/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -22,11 +23,32 @@ public class BookController {
     public @ResponseBody
     List<Book> getBooks(){return repository.findAll();}
 
+    //Get by ID
+    @GetMapping("/{id}")
+    public @ResponseBody Book findById(@PathVariable Long id){
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
     //Create one
     @PostMapping
     public ResponseEntity<Book> createCustomer(@RequestBody Book newBook){
         return new ResponseEntity<>(repository.save(newBook), HttpStatus.CREATED);
     }
 
+    @PutMapping("/{id}")
+    public @ResponseBody Book updateBook(@PathVariable Long id, @RequestBody Book updatedBook){
+        Book book = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if(updatedBook.getTitle() != null) book.setTitle(updatedBook.getTitle());
+        if(updatedBook.getAuthor() != null) book.setAuthor(updatedBook.getAuthor());
+
+        return repository.save(book);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> destroyBook(@PathVariable Long id){
+        repository.deleteById(id);
+        return new ResponseEntity<>("Book" + id +"Destroyed",HttpStatus.OK);
+    }
 
 }
