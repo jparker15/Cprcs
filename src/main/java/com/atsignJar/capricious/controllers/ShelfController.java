06378e6c2a674d0b92229e72a7d5158170d79719
Jeremy <jeremyeparker@yahoo.com>
2021-12-04 23:47:04 -0500

    restore shelf controller

--- src/main/java/com/atsignJar/capricious/controllers/ShelfController.java
+++ src/main/java/com/atsignJar/capricious/controllers/ShelfController.java
@@ -1,31 +1,42 @@
-//package com.atsignJar.capricious.controllers;
-//
-//import com.atsignJar.capricious.models.Shelf;
-//import com.atsignJar.capricious.repositories.ShelfRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import java.util.List;
-//
-//@CrossOrigin
-//@RestController
-//@RequestMapping("/api/library")
-//public class ShelfController {
-//
-//    @Autowired
-//    private ShelfRepository repository;
-//
-//    @GetMapping
-//    public @ResponseBody
-//    List<Shelf> getAllLibraries(){return repository.findAll();}
-//
-//    @GetMapping("/{id}")
-//    public @ResponseBody
-//    Shelf findById(@PathVariable Long id){
-//        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-//    }
-//
-//
-//}
+package com.atsignJar.capricious.controllers;
+
+import com.atsignJar.capricious.models.Shelf;
+import com.atsignJar.capricious.repositories.ShelfRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/libraries")
+public class ShelfController {
+
+    @Autowired
+    private ShelfRepository repository;
+
+    @GetMapping
+    public @ResponseBody
+    List<Shelf> getAllLibraries(){return repository.findAll();}
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Shelf> getById(@PathVariable Long id){
+        Optional<Shelf> shelf = repository.findById(id);
+
+        if(shelf.isEmpty()){
+            return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+        }
+
+        return  new ResponseEntity<>(shelf.get(),HttpStatus.OK);
+    }
+
+    @PostMapping
+    public Shelf createOne(@RequestBody Shelf newShelf){
+        return  repository.save(newShelf);
+    }
+
+}
