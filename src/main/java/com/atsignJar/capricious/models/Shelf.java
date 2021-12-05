94f1904f715f68cc003e325b236964aa2a8d8b44
Jeremy <jeremyeparker@yahoo.com>
2021-12-04 23:47:19 -0500

    remove List of books

--- src/main/java/com/atsignJar/capricious/models/Shelf.java
+++ src/main/java/com/atsignJar/capricious/models/Shelf.java
@@ -1,39 +1,36 @@
-//package com.atsignJar.capricious.models;
-//
-//import javax.persistence.*;
-//import java.util.List;
-//
-//@Entity
-//public class Shelf {
-//    @Id
-//    @GeneratedValue
-//    private Long id;
-//    private String name;
-//    private List <Book> books;
-//
-//
-//    public Shelf(){
-//    }
-//
-//    public Shelf(String name) {
-//        this.name = name;
-//    }
-//
-//
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//}
+package com.atsignJar.capricious.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Shelf {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Shelf(){
+    }
+
+    public Shelf(String name) {
+        this.name = name;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
